3abaad7f80563982ad24fd1bd34284190a04cc77
Robin <robin.sliet@gmail.com>
2024-04-06 23:19:38 +0530

    updated header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -37,7 +37,8 @@ function Header() {
   ];
   return (
     <>
-      <header className="py-3 shadow bg-gray-500">
+      {/* <header className="py-3 shadow bg-gray-500"> */}
+      <div className="py-3 shadow bg-gray-500">
         <Container>
           <nav className="flex">
             <div className="mr-4 ">
@@ -68,7 +69,8 @@ function Header() {
             </ul>
           </nav>
         </Container>
-      </header>
+      </div>
+      {/* </header> */}
     </>
   );
 }
