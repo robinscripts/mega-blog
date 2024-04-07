c3eae9e843b037a5750f048fae46fb60734899bf
Robin <robin.sliet@gmail.com>
2024-04-07 22:37:48 +0530

    updated readme and logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width = '100px'}) {
   return (
     <div>
-      <img src="src\assets\Mega Blogs.png" className='rounded-full' width={width} alt="" />
+      <img src="../assets/Mega Blogs.png" className='rounded-full' width={width} alt="" />
     </div>
   )
 }
